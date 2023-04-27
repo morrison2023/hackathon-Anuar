b2137c7b7a66280b717da9a6dd011d5c02794fb4
islamka2 <islamkaawad@gmail.com>
2023-04-27 14:53:49 +0600

    first

---
+++ script.js
@@ -0,0 +1,14 @@
+$(function () {
+  let header = $(".header");
+
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 1) {
+      header.addClass("header_fixed");
+    } else {
+      header.removeClass("header_fixed");
+    }
+  });
+});
+AOS.init({
+  duration: 1200,
+});
